ca52bc5bfc914683828a41208f6cf89b99ef34f9
morozovadiana <deanamorozova@mail.ru>
2026-05-15 18:17:03 +0300

    refactor homework 25 smartscenarios

--- app/src/androidTest/java/org/homework/lesson25/CloseBannersTest.kt
+++ app/src/androidTest/java/org/homework/lesson25/CloseBannersTest.kt
@@ -1,23 +1,38 @@
 package org.homework.lesson25
 
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import com.kaspersky.components.alluresupport.withForcedAllureSupport
+import com.kaspersky.kaspresso.kaspresso.Kaspresso
+import com.kaspersky.kaspresso.testcases.api.testcase.TestCase
 import org.homework.lesson18.h.homework.ExploreScreen
+import org.homework.lesson18.h.homework.ExploreScreen.featureArticle
 import org.homework.lesson18.h.lesson.OnboardingScreen
 import org.homework.lesson19.BaseTest
 import org.homework.lesson19.ext.action
+import org.homework.lesson19.ext.verify
+import org.homework.lesson22.homework.BottomSheetScreen
+import org.homework.lesson22.homework.FeatureArticleScreen
+import org.junit.Rule
 import org.junit.Test
+import org.wikipedia.main.MainActivity
 
-class CloseBannersTest : BaseTest() {
+class CloseBannersTest: TestCase(kaspressoBuilder = Kaspresso.Builder.withForcedAllureSupport()) {
+
+    @get:Rule
+    val testRule = ActivityScenarioRule(MainActivity::class.java)
 
     @Test
     fun checkCloseBannersTest() {
-        run{
+        run {
             action.click(OnboardingScreen.skipButton)
-            ExploreScreen.topReadBlock {
-                action.click(title)
-                topReadBlockByIndex(0) {
-                    action.click(textNumber)
-
-                }
+            featureArticle(1) {
+                action.click(this)
+            }
+            FeatureArticleScreen.navBarWidget {
+                action.click(theme)
+            }
+            BottomSheetScreen.readingWidget {
+                verify.isDisplayed(this)
             }
         }
     }
